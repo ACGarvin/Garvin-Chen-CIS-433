5e280854c1c31b42529c1df2ac2f72c22b758b78
ACGarvin <agarvin@cs.uoregon.edu>
2017-03-04 23:04:52 -0800

    Add files via upload
    Added the keys for RSA.  So now the message and AES are encoded into the QR code.

--- src/io/nayuki/qrcodegen/GeneratorFrame.java
+++ src/io/nayuki/qrcodegen/GeneratorFrame.java
@@ -1,85 +1,148 @@
 package io.nayuki.qrcodegen;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+
 import java.io.File;
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
 import javax.imageio.ImageIO;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 import javax.xml.bind.DatatypeConverter;
 
 public class GeneratorFrame extends JFrame {
 
+	//These are all Frame buttons and labels
 	private final JButton _GenerateButton = new JButton("Generate QR Code");
 	protected JTextField textField = new JTextField(50);
+	protected JLabel inputText = new JLabel("Type message to encode (less than 200 characters)");
 	// File imgFile = new File("QRCode.png");
-	SecretKey secKey = getSecretEncryptionKey();
 	
+	//Variables for filepaths for the public and private keys.
+	String publicKeyFilePath = "PublicKey.key";
+	String privateKeyFilePath = "PrivateKey.key";
+	
+	//This is the secKey for the AES encryption.  Move this to the action listener when you figure
+	//out how to append it to the message.  Needs to be new each message.
+	
+	SecretKey secKey = getSecretEncryptionKey();
+	byte[] secKeyBytes = secKey.getEncoded();
+	String secKeyString = bytesToHex(secKeyBytes);
+	
+
 	public GeneratorFrame() throws Exception {
 		super("QR Generator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 
 		JPanel textPanel = new JPanel(new FlowLayout());
+		textPanel.setPreferredSize(new Dimension(100, 65));
+		textPanel.add(inputText);
 		textPanel.add(textField);
 		textPanel.add(_GenerateButton);
 
 		add(textPanel, BorderLayout.SOUTH);
 		JLabel QRImage = new JLabel();
 		add(QRImage, BorderLayout.CENTER);
-		//SecretKey secKey = getSecretEncryptionKey();
-
+		// SecretKey secKey = getSecretEncryptionKey();
+		
 		_GenerateButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String text = textField.getText();
-				byte[] cipherText = null;
-				try {
-					cipherText = encryptText(text, secKey);
-				} catch (Exception e2) {
-					// TODO Auto-generated catch block
-					e2.printStackTrace();
-				}
-				System.out.print("Message to be encrypted is \n");
-				System.out.print(text + " \n");
-				String encryptedTextInHex = bytesToHex(cipherText);
-				System.out.print("Encrypted message: \n");
-				System.out.print(encryptedTextInHex);
-				System.out.print("\n");
-				byte[] attemptToDecrypt = hexToBytes(encryptedTextInHex);
-				String decryptedText = null;
-				try {
-					decryptedText = decryptText(attemptToDecrypt, secKey);
-				} catch (Exception e2) {
-					// TODO Auto-generated catch block
-					e2.printStackTrace();
-				}
-				System.out.print("Decrypted message: \n");
-				System.out.print(decryptedText);
-				try {
-					doBasicDemo(encryptedTextInHex);
-					BufferedImage image = ImageIO.read(new File("QRCode.png"));
-					QRImage.setIcon(new ImageIcon(image));
+				if (text.length() > 200) {
+					JOptionPane.showMessageDialog(null, "There is more than 200 Characters!");
+				} else {
 
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					//fill out text to be 200 characters in length
+					String formattedText = String.format("%1$-200s", text);
+					byte[] cipherText = null;
+					try {
+						cipherText = encryptText(formattedText, secKey);
+					} catch (Exception e2) {
+						// TODO Auto-generated catch block
+						e2.printStackTrace();
+					}
+					System.out.print("Message to be encrypted is: " + formattedText + "\n");
+					System.out.print("Size of Messagee is: " + formattedText.length() + "\n");
+					String encryptedTextInHex = bytesToHex(cipherText);
+					System.out.print("Encrypted message: " + encryptedTextInHex + "\n");
+					System.out.print("Size of Encrypted message: " + encryptedTextInHex.length() + "\n");
+					
+					//Encrypt the AES Key
+					byte [] secKeyBytes = secKey.getEncoded();
+					try {
+						encryptRSA(secKeyBytes);
+					} catch (Exception e3) {
+						// TODO Auto-generated catch block
+						e3.printStackTrace();
+					}
+					//convert AES Key to string
+					String encryptedAESinHex = bytesToHex(secKeyBytes);
+					
+					String combinedEncodes = encryptedTextInHex + encryptedAESinHex;
+					System.out.print("Encrypted message and Key: " + combinedEncodes + "\n");
+
+
+					
+					byte[] attemptToDecrypt = hexToBytes(encryptedTextInHex);
+					String decryptedText = null;
+					try {
+						decryptedText = decryptText(attemptToDecrypt, secKey);
+					} catch (Exception e2) {
+						// TODO Auto-generated catch block
+						e2.printStackTrace();
+					}
+					System.out.print("Decrypted message: ");
+					System.out.print(decryptedText);
+					System.out.print("\nTrying to append key to message before QR Code \n");
+					System.out.print("Size of AES Key as string: " + secKeyString.length() + "\n\n");
+
+					try {
+						doBasicDemo(combinedEncodes);
+						BufferedImage image = ImageIO.read(new File("QRCode.png"));
+						QRImage.setIcon(new ImageIcon(image));
+
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
 				}
 			}
 		});
 		// set the size of the panel
-		setSize(800, 500);
+		setSize(800, 800);
 	}
 
 	private static void doBasicDemo(String arg) throws IOException {
@@ -149,9 +212,109 @@ public class GeneratorFrame extends JFrame {
 	private static String bytesToHex(byte[] hash) {
 		return DatatypeConverter.printHexBinary(hash);
 	}
-	
-	private static byte[] hexToBytes(String hex){
+
+	private static byte[] hexToBytes(String hex) {
 		return DatatypeConverter.parseHexBinary(hex);
 	}
-}
+	
+	public byte[] encryptAES(byte[] aesKey) throws Exception {
 
+		Cipher cipher;
+
+		byte[] cipherData = null;
+
+		cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+		cipher.init(Cipher.ENCRYPT_MODE, loadPublicKey());
+
+		cipherData = cipher.doFinal(aesKey);
+
+		return cipherData;
+
+		}
+	
+	public void generateKeyPair(String publicFilePath, String privateFilePath) throws Exception {
+
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+
+		SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+
+		keyGen.initialize(1024, random);
+
+		KeyPair generatedKeyPair = keyGen.genKeyPair();
+
+		savePublicKey(generatedKeyPair.getPublic(), publicFilePath);
+
+		savePrivateKey(generatedKeyPair.getPrivate(), privateFilePath);
+
+		}
+
+		public void savePublicKey(PublicKey key, String filePath) throws Exception {
+
+		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(
+
+		key.getEncoded());
+
+		FileOutputStream fos = new FileOutputStream(filePath);
+
+		fos.write(x509EncodedKeySpec.getEncoded());
+
+		fos.close();
+
+		}
+
+		private void savePrivateKey(PrivateKey key, String filePath) throws Exception {
+
+		PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(
+
+		key.getEncoded());
+
+		FileOutputStream fos = new FileOutputStream(filePath);
+
+		fos.write(pkcs8EncodedKeySpec.getEncoded());
+
+		fos.close();
+
+		}
+		
+
+			private PublicKey loadPublicKey() throws Exception {
+
+			File filePublicKey = new File(publicKeyFilePath);
+
+			FileInputStream fis = new FileInputStream(filePublicKey);
+
+			byte[] encodedPublicKey = new byte[fis.available()];
+
+			fis.read(encodedPublicKey);
+
+			fis.close();
+
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
+			X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(
+
+			encodedPublicKey);
+
+			PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+
+			return publicKey;
+
+			}
+			
+			public byte[] encryptRSA(byte[] aesKey) throws Exception {
+
+				Cipher cipher;
+
+				byte[] cipherData = null;
+
+				cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+				cipher.init(Cipher.ENCRYPT_MODE, loadPublicKey());
+
+				cipherData = cipher.doFinal(aesKey);
+
+				return cipherData;
+
+				}
+}
